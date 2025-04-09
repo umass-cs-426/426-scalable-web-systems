f56b787f81f112a5fb9c22c220ebcf8808dda37a
richards <richards@cs.umass.edu>
2025-04-09 16:53:58 +0000

    udpate

--- modules/06-docker-basics/lectures/06-lecture-02/06-lecture-02-code/01-dockerized/index.ts
+++ modules/06-docker-basics/lectures/06-lecture-02/06-lecture-02-code/01-dockerized/index.ts
@@ -1,13 +1,22 @@
 // index.ts
 import express from 'express'
+import fs from 'fs'
+
+// Check if running in Docker container
+const inDocker = fs.existsSync('/.dockerenv');
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (_req, res) => {
-  res.send('Hello from a TypeScript Express server! 🚀')
+  const message = `
+  Hello from a TypeScript Express server! 🚀<br>
+  Am I in a Docker container? ${inDocker ? 'Yes!' : 'No.'}
+  `;
+  
+  res.send(message);
 })
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
+  console.log(`Server is running on Port ${port}!`)
 })
